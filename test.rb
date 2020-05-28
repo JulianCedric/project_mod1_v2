afd67ff22b6d822e0bfde9effc70647d861c65d3
JulianCedric <julian.pormentilla@gmail.com>
2020-05-28 13:17:55 -0400

    We're done with entire user flow. Next is coding each out.

--- test.rb
+++ test.rb
@@ -9,12 +9,28 @@
     # User is shown a menu of all 7 Suits. 
     # User is asked which suit he/she wants to see first.   
     # User is asked to enter the number next to each suit name he/she wants to see first.        
-        # User enters '3' for Hulkbuster suit.
+        # User enters '1' for Hulkbuster suit.
         # User is shown Hulkbuster's name, superpower, and the two villains he can fight with this suit.
         # User is shown a message: "You can unlock this suit by adding 3 movies to your movies list."
         # User is asked: "Do you want to start adding movies now?"
-            # Please enter 'Y' or 'N'. 
+            # Please enter 'Y' to be directed to the movie options screen. 
+            # Please enter 'N' to be directed back to the suits menu. 
                 # User enters 'Y' and is directed straight to the movie options screen. 
-            # If user enters 'N', he/she will be directed back to suits menu. 
     # User is shown a menu of all MCU movies.
-    
+    # User is asked which of the following movies he/she has seen. 
+    # User is asked to enter the number next to the first movie he/she has seen. 
+        # User enters '1' for 'Ironman 1' movie. 
+            # User sees a message: "You've just added your first movie."
+            # User sees a message: "You now have 1 movie points"
+            # User sees a message: "Add 2 more movies to add your first Ironman suit."
+            # User is asked: "Do you want to add them now?"
+                # Please enter 'Y' to be directed back to the movie menu to add your next movie.
+                # Please enter 'N' to be directed back to the main menu. 
+                    # User enters 'Y' and is directed back to the movie menu. 
+            # User is prompted with this message: "What's the second movie you've seen and want to add?"
+            # User is prompted to add third movie.
+            # User adds first Ironman Suit (Hulkbuster) and unlocks first two villains.
+
+# For demo, user is shown how to update account like name and nickname. 
+# To end demo, user is shown how to delete their account. 
+
