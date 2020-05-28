00bcb55cf99b946bf2a7ae23ba50691fda8657bb
JulianCedric <julian.pormentilla@gmail.com>
2020-05-28 12:57:09 -0400

    Malcolm and I finished user flow of main options menu and suit options menu.

--- test.rb
+++ test.rb
@@ -5,9 +5,16 @@
 # User is asked to select one of the two options.
     # Please enter '1' to browse all suits.
     # Please enter '2' to add Marvel movies you've seen. 
-# User enters '1'.
+# User enters '1' and enters browse suits branch.
     # User is shown a menu of all 7 Suits. 
     # User is asked which suit he/she wants to see first.   
-    # User is asked to enter the number next to each suit name.
-        # User enters '3' for Thanos.
-        # 
+    # User is asked to enter the number next to each suit name he/she wants to see first.        
+        # User enters '3' for Hulkbuster suit.
+        # User is shown Hulkbuster's name, superpower, and the two villains he can fight with this suit.
+        # User is shown a message: "You can unlock this suit by adding 3 movies to your movies list."
+        # User is asked: "Do you want to start adding movies now?"
+            # Please enter 'Y' or 'N'. 
+                # User enters 'Y' and is directed straight to the movie options screen. 
+            # If user enters 'N', he/she will be directed back to suits menu. 
+    # User is shown a menu of all MCU movies.
+    
